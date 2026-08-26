db3bf2686b3de1da2732023cacaf9a849100c759
Sanjana23435 <karemoresanju@gmail.com>
2026-08-26 15:37:51 +0530

    Delete Que2.js

--- Que2.js
+++
@@ -1,9 +0,0 @@
-const printArray = function(array) {
-    array.forEach(function(value) {
-        console.log(value);
-    });
-};
-
-// Example usage:
-const myArray = [1, 2, 3, 4, 5];
-printArray(myArray);
