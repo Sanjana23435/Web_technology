045c9b14dfd796d931f490a9b619fbc8e65e5f34
Sanjana23435 <karemoresanju@gmail.com>
2026-08-26 15:35:35 +0530

    Delete Que1.js

--- Que1.js
+++
@@ -1,9 +0,0 @@
-function printArray(arr) {
-    for (let i = 0; i < arr.length; i++) {
-        console.log(arr[i]);
-    }
-}
-
-// Example usage:
-const myArray = [1, 2, 3, 4, 5];
-printArray(myArray);
